6c6e8bbafc87d730263fb2e61455667991909c3a
gasolina90 <moralerick90@gmail.com>
2023-11-11 18:24:39 -0700

    updated Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
+
+public class Program{
+    static void Main(){
+        // GLOBAL VARIABLES
+        string myChoice = "";
+        string myEntry;
+        string specDate;
+        // System.IO.StreamWriter outputFile;
+
+        // Constructors
+        Journal journal = new Journal();
+        Interactor interactor = new Interactor();
+
+
+
+        journal.display_prompt();
+        myEntry = interactor.get_prompt_answer();
+        // specDate = interactor.get_date();
+
+        journal.save_journal("Entries.txt");
+
+        // journal.display_whole_journal(outputFile);
+
+            
+            
+            
+            // Main logic loop
+            while (myChoice != "6"){
+                Console.WriteLine();
+                Console.WriteLine(" - - - - - - - Main Menu - - - - - - - -");
+                Console.WriteLine("========================================");
+                Console.WriteLine("1) Write a new entry");
+                Console.WriteLine("2) Display Journal");
+                Console.WriteLine("3) Load Journal from file");
+                Console.WriteLine("4) Save Journal to file");
+                Console.WriteLine("5) Display Journal");
+                Console.WriteLine("6) Quit");
+                Console.WriteLine();
+                
+                myChoice = Console.ReadLine();
+
+                if (myChoice == "1"){
+                    journal.display_prompt();
+                    myEntry = interactor.get_prompt_answer();
+                    specDate = interactor.get_date();
+                }
+
+                else if (myChoice == "2"){
+                    journal.display_whole_journal(outputFile);
+                }
+
+            
+            else if (myChoice == "3"){
+                journal.load_journal("Entries.txt");
+            }
+
+            else if (myChoice == "4"){
+                outputFile = journal.save_journal();
+            }
+
+        }
+
     }
 }
